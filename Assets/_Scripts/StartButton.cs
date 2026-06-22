7207c1c3ebf150339bdc33281c2d80c5f2c0665c
Wolfyl <lasonenathan@gmail.com>
2026-06-22 13:08:37 -0500

    i'm trying everythign
    I don't know what i'm doing, but i need to make something show on the screen

---
+++ Assets/_Scripts/StartButton.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+  public void OnStartButtonClick()
+  {
+        // Add your logic here for what happens when the start button is clicked
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Scripts/System/Dialogue/DialogueContoller.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialogueContoller : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText, nameText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
